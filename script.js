a1b60b1c7d46f4131b01440716b28dcaf2ce702c
Ferdy Macleod <ferdymacleod21@gmail.com>
2019-11-21 14:56:34 +1000

    added todolist thingy

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-window.addEventListener('DOMContentLoaded', function() {
-    let container = document.querySelector('.container')
+document.addEventListener('DOMContentLoaded', function() {
+    let image = document.querySelector('.pic-of-day')
     let iframe = document.querySelector('iframe')
     let title = document.querySelector('.title')
     let desc = document.querySelector('.description')
+    let form = document.querySelector('.form')
+    
 
     $.getJSON("https://api.nasa.gov/planetary/apod?api_key=ewOaDXWglBEsQAKX2SApET8IsuyxxW6hCydetUE3")
 
@@ -10,8 +12,10 @@ window.addEventListener('DOMContentLoaded', function() {
         console.log(response)
         if (`${response.url}`.includes("youtube")) {
             iframe.src = `${response.url}`
+            image.classList.add('hidden')
         } else {
-            container.setAttribute("background-url", `${response.url}`)
+            image.src = `${response.url}`
+            iframe.classList.add('hidden')
         }
         title.innerHTML = `${response.title}`
         desc.innerHTML = `${response.explanation}`
@@ -21,4 +25,25 @@ window.addEventListener('DOMContentLoaded', function() {
     .catch(function(response) {
         console.log('didnt work');
     });
-}) 
+
+    form.addEventListener('submit', () => {
+        console.log('ah')
+    })
+
+    
+    
+})
+let list = document.querySelector('.list')
+let items = []
+
+function functionName() {
+    items.push(document.getElementById('firstname').value)
+    console.log(items)
+    items.forEach(displayItems)
+}
+
+function displayItems(item, index) {
+    let li = document.createElement('li')
+    li.innerHTML = item
+    list.appendChild(li)
+}
